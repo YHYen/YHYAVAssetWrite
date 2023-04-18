bde8bc2a6aa2963bc32590bd622d94556fe92209
YHYen <cloudyhyen@gmail.com>
2023-04-18 21:38:49 +0800

    Initial Commit

---
+++ YHYAVAssetWrite/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  YHYAVAssetWrite
+//
+//  Created by 顏逸修 on 2023/4/18.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ YHYAVAssetWrite/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  YHYAVAssetWrite
+//
+//  Created by 顏逸修 on 2023/4/18.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
